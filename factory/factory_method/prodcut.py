392096ccac23c2ec921be95899d14ae6c184dd49
PIW <loveelf1@naver.com>
2023-09-06 17:41:42 +0900

    ADD Factory Method

---
+++ factory/factory_method/prodcut.py
@@ -0,0 +1,22 @@
+from abc import ABCMeta, abstractclassmethod
+
+class Section(metaclass=ABCMeta):
+    @abstractclassmethod
+    def describe(self):
+        pass
+
+class PersonalSection(Section):
+    def describe(self):
+        print("Personal Section")
+
+class AlbumSection(Section):
+    def describe(self):
+        print("Album Section")
+
+class PatentSection(Section):
+    def describe(self):
+        print("Patent Section")
+
+class PublicationSection(Section):
+    def describe(self):
+        print("Publication Section")        
